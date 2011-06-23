d0199e2704f0e1cb37ee2dad1bc82f3c3ee31f12
myfreeweb <me@myfreeweb.ru>
2011-06-23 20:27:54 +0400

    Detecting base price

--- priceid.c
+++ priceid.c
@@ -3,8 +3,35 @@
 #define true  1
 #define false 0
 
-char* id(char *tp, int price, int charisma, int sucker, int selling) {
-  /* TODO */
+char *id(char *tp, int price, int charisma, int sucker, int selling) {
+  float resprice;
+  if (selling == true) {
+    if (sucker == true) {
+      resprice = price / 100.0 * 3.0;
+    } else {
+      resprice = price * 0.5;
+    }
+  } else {
+    if (sucker == true) {
+      resprice = price * 1.33;
+    } else {
+      resprice = price * 1.0;
+    }
+    if (charisma < 6) {
+      resprice = resprice * 2.0;
+    } else if (charisma < 8) {
+      resprice = resprice * 1.5;
+    } else if (charisma < 11) {
+      resprice = resprice * 1.33;
+    } else if (charisma > 15) {
+      resprice = resprice * 0.75;
+    } else if (charisma == 18) {
+      resprice = resprice * 0.67;
+    } else if (charisma > 18) {
+      resprice = resprice * 0.5;
+    }
+  }
+  return "";
 }
 
 char *lslice(char *str, int beg) {
